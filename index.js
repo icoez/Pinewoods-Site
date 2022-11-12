d9c212cd57fbcaf28dfd96fd1f01433d901f9270
icoez <78701335+icoez@users.noreply.github.com>
2022-11-12 19:24:27 +0000

    Very Basic weather implementation

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-window.onload = function(){
-    const menuButton = $('.hamburger');
-    const mobileNav = $('.mobile-nav');
-    menuButton.click(function(){
-        $(menuButton).toggleClass('is-active');
-        $(mobileNav).toggleClass('is-active');
-    })
-}
+window.onload = function () {
+  const menuButton = $(".hamburger");
+  const mobileNav = $(".mobile-nav");
+  menuButton.click(function () {
+    $(menuButton).toggleClass("is-active");
+    $(mobileNav).toggleClass("is-active");
+  });
+};
+let weatherTemp = $(".temp");
+let weatherDesc = $(".weather-description");
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.50&lon=-84.71&appid=7d1f2802046e058ab87b457b6417030f&units=imperial"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let tempValue = data["main"]["temp"];
+    let descValue = data["weather"]["description"];
+    $(weatherTemp).text(Math.round(tempValue));
+    $(weatherDesc).text(descValue);
+  });
