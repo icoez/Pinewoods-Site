c36ab740a3bcb2867ad01b561c948ff44f0ee9d5
icoez <78701335+icoez@users.noreply.github.com>
2022-11-15 18:43:19 +0000

    Full Weather System Working

--- index.js
+++ index.js
@@ -39,14 +39,29 @@ fetch(
 
     let hours = date.getHours();
     let hoursBeforeNextDay = 24 - hours;
-    let iconArray = [[], [], []];
-    let minArray = [Infinity, Infinity, Infinity];
-    let maxArray = [-Infinity, -Infinity, -Infinity];
+    let iconArray = [[], [], [], []];
+    let minArray = [Infinity, Infinity, Infinity, Infinity];
+    let maxArray = [-Infinity, -Infinity, -Infinity, -Infinity];
+    let windArray = [0, 0, 0, 0];
 
-    let windArray = [0, 0, 0];
     for (let i = 0; i < 8; i++) {
+      //?Day 1 stuff:
+      if (i < hoursBeforeNextDay / 3) {
+        let day1CurrentMin = data["list"][i]["main"]["temp_min"];
+        let day1CurrentMax = data["list"][i]["main"]["temp_max"];
+        let day1CurrentWind = data["list"][i]["wind"]["speed"];
+        let day1CurrentIcon = data["list"][i]["weather"][0]["icon"].slice(0, 2);
+        iconArray[0].push(day1CurrentIcon);
+        if (day1CurrentMin < minArray[0]) {
+          minArray[0] = day1CurrentMin;
+        }
+        if (day1CurrentMax > maxArray[0]) {
+          maxArray[0] = day1CurrentMax;
+        }
+        windArray[0] += day1CurrentWind;
+      }
       //?Day 2 stuff:
-      let day2Index = i;
+      let day2Index = i + hoursBeforeNextDay / 3;
       let day2CurrentMin = data["list"][day2Index]["main"]["temp_min"];
       let day2CurrentMax = data["list"][day2Index]["main"]["temp_max"];
       let day2CurrentWind = data["list"][day2Index]["wind"]["speed"];
@@ -54,16 +69,16 @@ fetch(
         0,
         2
       );
-      iconArray[0].push(day2CurrentIcon);
-      if (day2CurrentMin < minArray[0]) {
-        minArray[0] = day2CurrentMin;
+      iconArray[1].push(day2CurrentIcon);
+      if (day2CurrentMin < minArray[1]) {
+        minArray[1] = day2CurrentMin;
       }
-      if (day2CurrentMax > maxArray[0]) {
-        maxArray[0] = day2CurrentMax;
+      if (day2CurrentMax > maxArray[1]) {
+        maxArray[1] = day2CurrentMax;
       }
-      windArray[0] += day2CurrentWind;
+      windArray[1] += day2CurrentWind;
       //?Day 3 stuff:
-      day3Index = i + 8;
+      let day3Index = i + hoursBeforeNextDay / 3 + 8;
       let day3CurrentMin = data["list"][day3Index]["main"]["temp_min"];
       let day3CurrentMax = data["list"][day3Index]["main"]["temp_max"];
       let day3CurrentWind = data["list"][day3Index]["wind"]["speed"];
@@ -71,16 +86,16 @@ fetch(
         0,
         2
       );
-      iconArray[1].push(day3CurrentIcon);
-      if (day3CurrentMin < minArray[1]) {
-        minArray[1] = day3CurrentMin;
+      iconArray[2].push(day3CurrentIcon);
+      if (day3CurrentMin < minArray[2]) {
+        minArray[2] = day3CurrentMin;
       }
-      if (day3CurrentMax > maxArray[1]) {
-        maxArray[1] = day3CurrentMax;
+      if (day3CurrentMax > maxArray[2]) {
+        maxArray[2] = day3CurrentMax;
       }
-      windArray[1] += day3CurrentWind;
+      windArray[2] += day3CurrentWind;
       //?Day 4 stuff:
-      day4Index = i + 16;
+      let day4Index = i + hoursBeforeNextDay / 3 + 16;
       let day4CurrentMin = data["list"][day4Index]["main"]["temp_min"];
       let day4CurrentMax = data["list"][day4Index]["main"]["temp_max"];
       let day4CurrentWind = data["list"][day4Index]["wind"]["speed"];
@@ -88,24 +103,26 @@ fetch(
         0,
         2
       );
-      iconArray[2].push(day4CurrentIcon);
-      if (day4CurrentMin < minArray[2]) {
-        minArray[2] = day4CurrentMin;
+      iconArray[3].push(day4CurrentIcon);
+      if (day4CurrentMin < minArray[3]) {
+        minArray[3] = day4CurrentMin;
       }
-      if (day4CurrentMax > maxArray[2]) {
-        maxArray[2] = day4CurrentMax;
+      if (day4CurrentMax > maxArray[3]) {
+        maxArray[3] = day4CurrentMax;
       }
-      windArray[2] += day4CurrentWind;
+      windArray[3] += day4CurrentWind;
     }
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
       windArray[i] = Math.round((windArray[i] / 8) * 10) / 10;
       let modalIcon = getModalElement(iconArray[i]);
       let imgSelector;
       if (i == 0) {
-        imgSelector = $(".img-2");
+        imgSelector = $(".img-1");
       } else if (i == 1) {
-        imgSelector = $(".img-3");
+        imgSelector = $(".img-2");
       } else if (i == 2) {
+        imgSelector = $(".img-3");
+      } else if (i == 3) {
         imgSelector = $(".img-4");
       }
       switch (modalIcon) {
@@ -138,17 +155,11 @@ fetch(
           break;
       }
     }
-    $(".min-2").text(`Min - ${Math.round(minArray[0] * 10) / 10}`);
-    $(".max-2").text(`Max - ${Math.round(maxArray[0] * 10) / 10}`);
-    $(".wind-2").text(`Wind - ${windArray[0]}`);
-
-    $(".min-3").text(`Min - ${Math.round(minArray[1] * 10) / 10}`);
-    $(".max-3").text(`Max - ${Math.round(maxArray[1] * 10) / 10}`);
-    $(".wind-3").text(`Wind - ${windArray[1]}`);
-
-    $(".min-4").text(`Min - ${Math.round(minArray[2] * 10) / 10}`);
-    $(".max-4").text(`Max - ${Math.round(maxArray[2] * 10) / 10}`);
-    $(".wind-4").text(`Wind - ${windArray[2]}`);
+    for (let i = 0; i < 4; i++) {
+      $(`.min-${i + 1}`).text(`Min - ${Math.round(minArray[i] * 10) / 10} ℉`);
+      $(`.max-${i + 1}`).text(`Max - ${Math.round(maxArray[i] * 10) / 10} ℉`);
+      $(`.wind-${i + 1}`).text(`Avg. wind - ${windArray[i]} mph`);
+    }
   });
 function getModalElement(arr) {
   let nodeMap = {};
